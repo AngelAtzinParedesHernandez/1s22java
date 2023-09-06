a9ef6161c79d2b126516dedfa0dbc749d575a31c
AngelAtzinParedesHernandez <144190833+AngelAtzinParedesHernandez@users.noreply.github.com>
2023-09-05 19:48:39 -0600

    Add files via upload
    practica 1 y 2 del 5 de septiembre del 2023

---
+++ p1_1s22.java
@@ -0,0 +1,5 @@
+class p1_1s22{
+	public static void main(string[] args){
+		System.out.println("Estamos empezando a porgramar hola mundo");
+	}
+}
---
+++ p2_1s22.java
@@ -0,0 +1,10 @@
+class p2_1s22 {
+	public static void main(String[] args) {
+		System.out.printl("Angel Atzin Predes Hernandez");
+		System.out.printl("17 años");
+		System.out.printl("angel15dic@hotmail.com");
+		System.out.printl("fumar");
+		System.out.printl("Azul");
+		
+	}
+}
