25c92d1fef884cb69dbce1bb1ea7627d4ebd3dc9
AngelAtzinParedesHernandez <144190833+AngelAtzinParedesHernandez@users.noreply.github.com>
2023-09-08 18:49:03 -0600

    Add files via upload
    Practica 4 angel atzin paredes hernandez

---
+++ P·4_1s22.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class P4_1s22{
+	ppublic static void main(String[] args) {
+		Scannerteclado = new Scanner(System.in);
+		String temp1, temp2;
+		int num1, num2;
+		String resultado;
+
+		System.out.println("Ingresa el primer numero: ")
+		temp = teclado.nextline();
+		num1 = Integer.parseInt(temp);
+		System.outprintln("Ingresa el segindo numero: ")
+		temp = teclado.nextline();
+		num2 = Integer.parseInt(temp);
+		resultado = num1 + num2;
+		System.out.println("La suma es; " + resultado);
+	}
+}
